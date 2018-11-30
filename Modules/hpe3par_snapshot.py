30bdd2c7567a9f5b8ad5c53e355b61e1b0e44bf6
Farhan Nomani <farhan7500@gmail.com>
2018-11-30 10:54:25 +0530

    Addressed review

--- Modules/hpe3par_snapshot.py
+++ Modules/hpe3par_snapshot.py
@@ -526,9 +526,8 @@ than 19 characters", {})
 
     expiration_hours = convert_to_hours(expiration_time, expiration_unit)
     retention_hours = convert_to_hours(retention_time, retention_unit)
-    if not(not expiration_hours or not retention_hours):
-        if expiration_hours <= retention_hours:
-            return (False, False, "Expiration time must be \
+    if expiration_hours and retention_hours and expiration_hours <= retention_hours:
+        return (False, False, "Expiration time must be \
 greater than retention time for non zero values", {})
 
     try:
